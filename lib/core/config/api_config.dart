57ba3a7bf9479efa008680a452e70358a64ab167
Shawket Ibrahim <shawket.4@icloud.com>
2026-06-18 15:23:03 +0300

    Changes

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,2 +1,2 @@
-// const String kApiBaseUrl = 'https://sufrix.duckdns.org/api';
-const String kApiBaseUrl = 'http://localhost:8081';
+const String kApiBaseUrl = 'https://sufrix.duckdns.org/api';
+// const String kApiBaseUrl = 'http://localhost:8081';
